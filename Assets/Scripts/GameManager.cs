d01d0dfba57d7857e890636ff6ebbc8375e2415b
William AbouSharaf <liams_1998@hotmail.com>
2020-09-16 21:34:14 +0300

    Add GameManager to scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    [HideInInspector] public bool gameIsOn = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+       
+    }
+
+    public void StartGame()
+    {
+        gameIsOn = true;
+    }
+
+    public void WinGame()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoseGame()
+    {
+        gameIsOn = false;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
