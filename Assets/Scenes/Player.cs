c4a37f29ba64c2ffa5b86faf7d1157b9089450a4
William AbouSharaf <liams_1998@hotmail.com>
2020-09-19 19:04:50 +0300

    Add multiple gameobjects in scene + config to mobile display

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int startingAmountHealth = 3;
+    private int currentAmountHealth;
+
+    public int startingAmountOfbullets = 6;
+    private int currentAmountOfBullets;
+
+    private float currentPlayerSpeed = 1.0f;
+
+
+    void Start()
+    {
+        currentAmountHealth = startingAmountHealth;
+        currentAmountOfBullets = startingAmountOfbullets;    
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
